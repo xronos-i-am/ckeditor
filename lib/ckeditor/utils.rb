d8fc94ce6fcf79211545a679a5a64b4b83ce252c
glebtv <glebtv@gmail.com>
2014-02-11 04:40:54 +0400

    try to fix precompile 2

--- lib/ckeditor/utils.rb
+++ lib/ckeditor/utils.rb
@@ -98,7 +98,7 @@ module Ckeditor
           end
         end
 
-        paths.map { |path| path.relative_path_from(relative_folder).to_s }
+        paths.map { |path| path.relative_path_from(relative_folder).to_s }.select { |path| path != 'ckeditor/ckeditor.js' }
       end
     end
   end
